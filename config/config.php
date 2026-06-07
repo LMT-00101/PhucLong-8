94f7152fb253046ecd7ad27cdf5e0cc351188249
LMT-00101 <la05.minhtrang@gmail.com>
2026-06-08 02:36:04 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,9 +3,9 @@
  * Mở kết nối đến CSDL sử dụng PDO
  */
 function pdo_get_connection(){
-    $dburl = "mysql:host=localhost;dbname=phlog;charset=utf8";
-    $username = 'root';
-    $password = '';
+    $dburl = "mysql:host=phlog-lmt-009.g.aivencloud.com;port=18766;dbname=phlog;charset=utf8";
+    $username = 'avnadmin';
+    $password = 'AVNS_Sk1B_i25czPfG_RICYG';
 
     $conn = new PDO($dburl, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
